fc1b949fbf1848942a810b10353e128d6a85e32a
Sebastian Lindau-Skands <47390147+Lynet101@users.noreply.github.com>
2024-05-21 09:55:05 +0200

    Add files via upload

--- dupe_rem.py
+++ dupe_rem.py
@@ -1,11 +1,11 @@
 import os
 import hashlib
 
-def find_duplicates(directory):
+def find_duplicates(directory) -> List:
     file_hashes = {}
     duplicate_files = []
 
-    for root, dirs, files in os.walk(directory):
+    for root, _, files in os.walk(directory):
         for filename in files:
             file_path = os.path.join(root, filename)
             with open(file_path, 'rb') as file:
@@ -17,11 +17,16 @@ def find_duplicates(directory):
 
     return duplicate_files
 
-def delete_duplicates(directory):
+def delete_duplicates(directory) -> None:
     duplicate_files = find_duplicates(directory)
     for file_path in duplicate_files:
         os.remove(file_path)
         print(f"{file_path} has been deleted.")
 
 # Usage
-delete_duplicates('./')
+def main() -> None:
+    path = input("Path to folder or files: ")
+    delete_duplicates(path)
+
+if __name__ == "__main__":
+    main()
